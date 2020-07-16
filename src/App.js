d8b2d879d5ff5d2cdb81d82fac4ce5c255e18581
Benjamin Levy <benjaminlevy@fb.com>
2020-07-16 17:00:04 -0400

    Got the map working

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar.js';
 import Map from './Map';
+import csvData from './police_reform_tracker_data_hack_1.csv';
+import * as d3 from 'd3';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-      <Map/>
-    </div>
-  );
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: null};
+  }
+
+  componentDidMount() {
+    // Loading data
+    d3.csv(csvData).then((data) => {
+      data.forEach(d => {
+        d.color_code = +d.color_code;
+      });
+      if (this.state.data !== data) {
+        this.setState({data: data});
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar/>
+        {this.state.data ? <Map data={this.state.data}/> : <div>Loading</div>}
+      </div>
+    )
+  }
 }
-
-export default App;
--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Chart } from 'react-google-charts';
 import * as d3 from 'd3';
-import csvData from './police_reform_tracker_data_hack_1.csv';
 import { select } from 'd3';
 
 const options = {
@@ -19,23 +18,21 @@ const createTooltipContent = (status, link, date) => {
     + '</div>';
 };
 
-const Map = () => {
+export default function Map(props) {
     const [mapData, setMapData] = React.useState([]);
     const [csvInfo, setCSVInfo] = React.useState([]);
     React.useEffect(() => {
-        d3.csv(csvData).then(function(csvData) {
-            console.log(csvData);
-            const cols = [["x", "y", { role: "tooltip", type: "string", p: { html: true } }]];
-            const rows = csvData.map(obj => [
-                obj.state,
-                Number(obj.color_code),
-                createTooltipContent(obj.color_code_alias, obj.source_link, obj.current_as_of),
-            ]);
-            setMapData(cols.concat(rows));
-            setCSVInfo(csvData);
-        });
+        const cols = [["x", "y", { role: "tooltip", type: "string", p: { html: true } }]];
+        const rows = props.data.map(obj => [
+            obj.state,
+            obj.color_code,
+            createTooltipContent(obj.color_code_alias, obj.source_link, obj.current_as_of),
+        ]);
+        setMapData(cols.concat(rows));
+        setCSVInfo(props.data);
     }, [])
     return (
+
         <div style={{margin: "0 auto"}} className={"map-container"}>
             <Chart
                 chartType="GeoChart"
@@ -46,19 +43,17 @@ const Map = () => {
                 legendToggle
                 chartEvents={[
                     {
-                      eventName: "select",
-                      callback: ({ chartWrapper }) => {
+                    eventName: "select",
+                    callback: ({ chartWrapper }) => {
                         const selection = chartWrapper.getChart().getSelection();
                         if (selection.length === 0) return;
                         const articleURL = csvInfo[selection[0].row].source_link;
                         if (articleURL !== "None Found")
                             window.open(articleURL, '_blank');
-                      }
                     }
-                  ]}
+                    }
+                ]}
             />
         </div>
     );
 }
-
-export default Map;
