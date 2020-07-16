7ab8b62267080ae1af13fd9b040f5aafaef03c6b
Benjamin Levy <benjaminlevy@fb.com>
2020-07-16 15:26:22 -0400

    Commenting out table for now

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/navbar.js';
+import Table from './components/table.js'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
     </div>
+    {/*<div>
+      <Table data=data/>
+    </div>*/}
   );
 }
 
