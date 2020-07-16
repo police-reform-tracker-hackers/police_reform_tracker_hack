ec9024db38b3f7cc28d78ca9039c64879c172880
Nila Selvaraj <nila.selvaraj@utexas.edu>
2020-07-16 17:23:55 -0500

    map styling & legend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar.js';
-import Map from './Map';
+import Map from './components/Map';
 import DataTable from './components/DataTable.js';
 import csvData from './police_reform_tracker_data_hack_1.csv';
 import * as d3 from 'd3';
---
+++ src/components/Map.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Chart } from 'react-google-charts';
+import './Map.css';
+
+const options = {
+    region: 'US',
+    displayMode: 'regions',
+    resolution: 'provinces',
+    tooltip: {isHtml: true},
+    legend: 'none',
+};
+
+const createTooltipContent = (status, link, date) => {
+    return '<div>'
+        +'<p>' +status +'</p>'
+        +'<p>' +date +'</p>'
+    + '</div>';
+};
+
+export default function Map(props) {
+    const [mapData, setMapData] = React.useState([]);
+    const [csvInfo, setCSVInfo] = React.useState([]);
+
+    const legendInfo = [
+        ["No Action Taken Yet", "rgb(239, 230, 220)"],
+        ["Commitment to Act", "rgb(128, 190, 122)"],
+        ["Bill Passed Or In Process", "rgb(16, 150, 24)"],
+    ];
+
+    React.useEffect(() => {
+        const cols = [["State", "Color", { role: "tooltip", type: "string", p: { html: true } }]];
+        const rows = props.data.map(obj => [
+            obj.state,
+            obj.color_code,
+            createTooltipContent(obj.color_code_alias, obj.source_link, obj.current_as_of),
+        ]);
+        setMapData(cols.concat(rows));
+        setCSVInfo(props.data);
+    }, [props.data]);
+
+    return (
+        <div className={"map-container"}>
+            <Chart
+                chartType="GeoChart"
+                data={mapData}
+                options={options}
+                legendToggle
+                width="90vw"
+                chartEvents={[
+                    {
+                        eventName: "select",
+                        callback: ({ chartWrapper }) => {
+                            const selection = chartWrapper.getChart().getSelection();
+                            if (selection.length === 0) return;
+                            const articleURL = csvInfo[selection[0].row].source_link;
+                            if (articleURL !== "None Found")
+                                window.open(articleURL, '_blank');
+                        }
+                    }
+                ]}
+            />
+            <div className={'map-legend'}>
+                <ul>
+                    {legendInfo.map(item => (
+                        <li key={item[0]}>
+                            <div style={{backgroundColor: item[1]}}></div>
+                            {item[0]}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
