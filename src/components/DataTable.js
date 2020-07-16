138fc612f45bddbdadf79656dfb3ca5376c4fcdb
Benjamin Levy <benjaminlevy@fb.com>
2020-07-16 17:17:10 -0400

    Table is up now

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -8,44 +7,38 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
 
 export default class DataTable extends React.Component {
-  const classes = useStyles();
-  const data = this.props.data;
-  const header = data.columns.map(((d, i) => {(
-              if (i === 0) {
-                return <TableCell>{d}</TableCell>
-              } else {
-                return <TableCell align="right">{d}</TableCell>
-              }
-              <TableCell>{d}</TableCell>
-            )}));
-  const body = data.map((row) => {
-    return (
-      <TableRow key={row.name}>
-        data.columns.map((d, i) => {
-          if (i === 0) {
-            return <TableCell>{d}</TableCell>
-          } else {
-            return <TableCell align="right">{d}</TableCell>
-          }
-      </TableRow>
-    )
+  render() {
+    const data = this.props.data;
+    const header = data.columns.map((d, i) => {
+      if (i === 0) {
+        return (<TableCell>{d}</TableCell>)
+      } else {
+        return (<TableCell align="right">{d}</TableCell>)
+      }
+    });
+    const body = data.map((row) => {
+      const cells = data.columns.map((d, i) => {
+        if (i === 0) {
+          return (<TableCell>{row[d]}</TableCell>)
+        } else {
+          return (<TableCell align="right">{row[d]}</TableCell>)
+        }
+      })
+      return (
+        <TableRow key={row.name}>{cells}</TableRow>
+      )
     })
-  });
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>{header}</TableRow>
-        </TableHead>
-        <TableBody>{body}</TableBody>
-      </Table>
-    </TableContainer>
-  );
+    return (
+      <TableContainer component={Paper}>
+        <Table aria-label="data table">
+          <TableHead>
+            <TableRow>{header}</TableRow>
+          </TableHead>
+          <TableBody>{body}</TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
 }
