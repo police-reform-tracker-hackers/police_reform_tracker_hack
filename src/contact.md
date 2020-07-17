1372a387c093893ec73dd838a729a8795cfdb456
Benjamin Levy <benjaminlevy@fb.com>
2020-07-17 18:30:32 -0400

    Added contact

---
+++ src/contact.md
@@ -0,0 +1,1 @@
+[info@policereformtracker.com](mailto:info@policereformtracker.com)
--- src/sections/SectionContainer.js
+++ src/sections/SectionContainer.js
@@ -4,9 +4,13 @@ import './SectionContainer.css'
 
 export default class SectionContainer extends React.Component {
     render() {
+        let style = {};
+        if (this.props.backgroundColor) {
+            style['background-color'] = this.props.backgroundColor;
+        }
         return (
             <section id={this.props.id}>
-                <div className="section">
+                <div className="section" style={style}>
                     <div className="component">
                         {this.props.component}
                     </div>
