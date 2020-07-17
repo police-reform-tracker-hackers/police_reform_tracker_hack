0979d5c268081aa62a54434e993a21aca348d55f
Benjamin Levy <benjaminlevy@fb.com>
2020-07-17 14:08:28 -0400

    Navbar mobile friendly

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,9 @@ import './NavBar.css';
 import NavItem from './NavItem.js';
 import {Link, animateScroll as scroll} from 'react-scroll';
 import {AppBar, Toolbar, Button} from '@material-ui/core';
-// import MenuIcon from '@material-ui/icons/Menu';
+import ResponsiveMenu from 'react-responsive-navbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 
 export default class NavBar extends React.Component {
@@ -11,44 +13,45 @@ export default class NavBar extends React.Component {
     //     super(props);
     // }
     render() {
-        return <AppBar
-        position="sticky"
-        className="navbar"
-        style={{ background: 'rgba(0, 0, 0, 0.2)', boxShadow: 'none'}}
-        >
-            <Toolbar>
-                {/*<IconButton
-                    edge="start"
-                    color="black"
-                    aria-label="menu"
-                >
-                    <MenuIcon />
-                </IconButton>*/}
-                <NavItem
-                    sectionId="map"
-                    sectionName="Map"
-                />
-                <NavItem
-                    sectionId="data"
-                    sectionName="Data"
-                />
-                <NavItem
-                    sectionId="form"
-                    sectionName="Submit an update"
-                />
-                <NavItem
-                    sectionId="takeaction"
-                    sectionName="Take action"
-                />
-                <NavItem
-                    sectionId="mission"
-                    sectionName="Mission"
-                />
-                <NavItem
-                    sectionId="contact"
-                    sectionName="Contact"
-                />
-            </Toolbar>
-        </AppBar>
+
+        return (
+            <div className="sticky">
+            <ResponsiveMenu
+                menuOpenButton={<MenuIcon/>}
+                menuCloseButton={<CloseIcon/>}
+                changeMenuOn="600px"
+                largeMenuClassName="navbar-lg"
+                smallMenuClassName="navbar-sm"
+                menu={
+                    <ul>
+                        <NavItem
+                            sectionId="map"
+                            sectionName="Map"
+                        />
+                        <NavItem
+                            sectionId="data"
+                            sectionName="Data"
+                        />
+                        <NavItem
+                            sectionId="form"
+                            sectionName="Submit an update"
+                        />
+                        <NavItem
+                            sectionId="takeaction"
+                            sectionName="Take action"
+                        />
+                        <NavItem
+                            sectionId="mission"
+                            sectionName="Mission"
+                        />
+                        <NavItem
+                            sectionId="contact"
+                            sectionName="Contact"
+                        />
+                    </ul>
+                }
+            />
+            </div>
+        )
     }
 }
