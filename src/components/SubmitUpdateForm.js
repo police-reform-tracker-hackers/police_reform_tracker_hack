7a560802362f712747b99e3cd51363a3967f7657
levyxx <levy.b.j.s@gmail.com>
2020-08-27 16:14:35 -0400

    Date field fixed on safari and form works

--- src/components/SubmitUpdateForm.js
+++ src/components/SubmitUpdateForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './SubmitUpdateForm.css';
-import { Card, Typography, Button } from '@material-ui/core';
+import { Card, Typography } from '@material-ui/core';
 
 const states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 
   'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 
@@ -14,16 +14,42 @@ const legStatuses = ['No Action Taken', 'Commitment to Act', 'Bill in Progress',
   'Bill Passed'];
 
 export default function SubmitUpdateForm() {
+  const question2entry = {
+    state: "entry.324818163",
+    legStatus: "entry.918943089",
+    link: "entry.1762728094",
+    date: "entry.267144032",
+    firstName: "entry.395727606",
+    lastName: "entry.1087655281",
+    email: "entry.1832616597"
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const cors = 'https://cors-anywhere.herokuapp.com/';
+    const googleFormURL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeHIWw6gyf5N9z5t-Inywv4Elce8cGsS-vbRtLKHy_yApH0sw/formResponse";
+    const data = {}
+    const urlData = []
+    Object.entries(question2entry).forEach(([q, e]) => {
+      data[e] = event.target[q].value;
+      urlData.push(`${e}=${event.target[q].value}`)
+    })
+    const dataStr = urlData.join('&');
+
+    window.open(googleFormURL + '?' + dataStr, '_blank');
+  }
+
     return (
         <Card className={"submit-update-form-container"}>
           <Typography component="h6" variant="h6">
             Submit a Legislative Update
           </Typography>
-            <form className={"submit-update-form"}
-              action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeHIWw6gyf5N9z5t-Inywv4Elce8cGsS-vbRtLKHy_yApH0sw/formResponse"
-              >
-              <p>State *</p>
-              <select name="entry.324818163">
+          <form
+              className={"submit-update-form"}
+              onSubmit={handleSubmit}
+          >
+            <p>State *</p>
+              <select name="state">
                 <option selected disabled>Choose</option>
                 {
                   states.map(state => <option key={state} value={state}>{state}</option>)
@@ -31,29 +57,29 @@ export default function SubmitUpdateForm() {
               </select>
 
               <p>Legislative Status *</p>
-              <select name="entry.918943089">
+              <select name="legStatus">
                 <option selected disabled>Choose</option>
                 {
                   legStatuses.map(status => <option key={status} value={status}>{status}</option>)
                 }
               </select>
 
-              <label for="link">Link to Your Source *</label>
-              <input name="entry.1762728094" type="text" id="link"/>
+              <label htmlFor="link">Link to Your Source *</label>
+              <input name="link" type="text" id="link"/>
 
-              <label for="date">Date of Your Source *</label>
-              <input name="entry.267144032" type="date"/>
+              <label htmlFor="date">Date of Your Source *</label>
+              <input name="date" type="date" placeholder="YYYY-MM-DD" />
 
-              <label for="first-name">Your First Name *</label>
-              <input name="entry.395727606" type="text" id="first-name"/>
+              <label htmlFor="firstName">Your First Name *</label>
+              <input name="firstName" type="text" id="first-name"/>
 
-              <label for="last-name">Your Last Name *</label>
-              <input name="entry.1087655281" type="text" id="last-name"/>
+              <label htmlFor="lastName">Your Last Name *</label>
+              <input name="lastName" type="text" id="last-name"/>
 
-              <label for="email">Best Email to Ask You a Follow-up Question, If Needed *</label>
-              <input name="entry.1832616597" type="text" id="email"/>
+              <label htmlFor="email">Best Email to Ask You a Follow-up Question, If Needed *</label>
+              <input name="email" type="text" id="email"/>
 
-              <input type="submit" value="Submit"></input>
+              <input type="submit"></input>
             </form>
         </Card>
     );
