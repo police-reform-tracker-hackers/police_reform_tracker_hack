4cf9bb68f3121e4430abdd5785bc67c1a8f1746d
Ben Levy <levy.b.j.s@gmail.com>
2020-09-26 10:42:10 -0400

    Adding head

---
+++ src/sections/Head.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+
+const Head = () => {
+    const meta = {
+        title: "Police Reform Tracker",
+        description: "Tracking reforms and commitments across state legislatures in the United States",
+        keywords: [
+            "police reform",
+            "police",
+            "police accountability",
+            "police reform tracker",
+            "police reform tracking",
+            "united states",
+            "united states of america",
+            "united states",
+            "usa",
+            "racial justice",
+            "racism",
+            "protests",
+            "legislation"
+        ]
+    }
+    return (
+        <Helmet>
+            <html lang="en"/>
+            <title>Police Reform Tracker</title>
+            <meta name="title" content={meta.title} />
+            <meta name="description" content={meta.description}/>
+            <meta name="keywords" content={meta.keywords.join(",")}/>
+            <meta name="robots" content="index, follow"/>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+            <meta name="language" content="English"/>
+
+            <meta property="og:title" content={meta.title} />
+            <meta property="og:description" content={meta.description} />
+            <meta property="og:image" content="img/map.png" />
+        </Helmet>
+    )
+}
+
+export default Head;
